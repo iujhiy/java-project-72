a4da9cef483665068e566d31dfcd6233576034eb
nikita <raknubov2@gmail.con>
2026-01-04 21:00:59 +0300

    add mock test

--- app/src/test/java/hexlet/code/controller/UrlsChecksControllerTest.java
+++ app/src/test/java/hexlet/code/controller/UrlsChecksControllerTest.java
@@ -1,6 +1,7 @@
 package hexlet.code.controller;
 
 import hexlet.code.BaseTestClass;
+import hexlet.code.util.NamedRoutes;
 import io.javalin.testtools.JavalinTest;
 import mockwebserver3.MockResponse;
 import mockwebserver3.MockWebServer;
@@ -10,6 +11,8 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class UrlsChecksControllerTest extends BaseTestClass {
     private static MockWebServer mockServer;
     private static String mockServerUrl;
@@ -17,7 +20,6 @@ public class UrlsChecksControllerTest extends BaseTestClass {
     @BeforeAll
     public static void setUpAll() throws IOException {
         mockServer = new MockWebServer();
-        mockServer.enqueue(new MockResponse.Builder().code(200).build());
         mockServer.start();
         mockServerUrl = mockServer.url("/").toString();
     }
@@ -30,9 +32,17 @@ public class UrlsChecksControllerTest extends BaseTestClass {
     }
 
     @Test
-    public void testUrlNotFound() {
+    public void testCreateUrl() {
         JavalinTest.test(app, (server, client) -> {
-            var requestBody = "urlId=999";
+            mockServer.enqueue(new MockResponse.Builder().code(200).build());
+            client.post("/urls", "url=" + mockServerUrl);
+            var checkResponse = client.post(NamedRoutes.urlPath(1));
+            assertThat(checkResponse.code()).isEqualTo(302);
+            var response = client.get(NamedRoutes.urlPath(1));
+            assertThat(response.body().string()).contains("200");
         });
     }
+
+
+
 }
