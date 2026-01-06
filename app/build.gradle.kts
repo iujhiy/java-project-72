de77cb0c853847bc7bff233d7ed8015bfa66577d
nikita <raknubov2@gmail.con>
2026-01-06 15:00:32 +0300

    mod build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,24 +35,24 @@ sonar {
 }
 
 dependencies {
-    implementation(platform("com.konghq:unirest-java-bom:4.5.1"))
-    implementation("com.konghq:unirest-java-core")
-    implementation("com.konghq:unirest-modules-jackson")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.27.3")
+    testImplementation("com.squareup.okhttp3:mockwebserver3:5.3.0")
+    testImplementation("com.h2database:h2:2.2.224")
+    implementation(platform("com.konghq:unirest-java-bom:4.5.1"))
+    implementation("com.konghq:unirest-java-core")
+    implementation("com.konghq:unirest-modules-jackson")
     implementation("io.javalin:javalin:6.6.0")
     implementation("io.javalin:javalin-bundle:6.6.0")
     implementation("io.javalin:javalin-rendering:6.6.0")
     implementation("org.slf4j:slf4j-simple:2.0.17")
     implementation("com.zaxxer:HikariCP:7.0.2")
-    implementation("com.h2database:h2:2.2.224")
     implementation("org.postgresql:postgresql:42.7.7")
     implementation("gg.jte:jte:3.2.1")
+    implementation("org.jsoup:jsoup:1.17.2")
     compileOnly ("org.projectlombok:lombok:1.18.30")
     annotationProcessor ("org.projectlombok:lombok:1.18.30")
-    testImplementation("com.squareup.okhttp3:mockwebserver3:5.3.0")
-    implementation("org.jsoup:jsoup:1.17.2")
 }
 
 tasks.jacocoTestReport {
