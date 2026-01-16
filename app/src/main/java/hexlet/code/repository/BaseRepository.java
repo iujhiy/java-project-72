1f4ad0fee90fea08aa17e5a34ae7c3488b5e8a2b
Никита <raknubov2@gmail.com>
2026-01-16 09:34:38 +0300

    explain BaseRepository.java

--- app/src/main/java/hexlet/code/repository/BaseRepository.java
+++ app/src/main/java/hexlet/code/repository/BaseRepository.java
@@ -6,5 +6,6 @@ public class BaseRepository {
     public static HikariDataSource dataSource;
 
     public BaseRepository() {
+        // этот класс нужен для централизованного подключения к HikariDataSource для репозиториев-наследников
     }
 }
